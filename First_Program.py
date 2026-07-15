939a8f464ada2df5cf3e1829c0536521a8d80201
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-15 11:35:17 +0530

    Calculate Some Areas.

---
+++ First_Program.py
@@ -0,0 +1,31 @@
+import math
+
+# radious = float(input("Enter the radious of the circle: "))
+# circum = 2*math.pi*radious
+# # print("The circumference of the circle is:",circum)
+# print(f"The circumference of the circle is {circum}")
+# print(f"The circumference of the circle is {round(circum,3)}")
+# print(f"The circumference of the circle is {math.floor(circum)}")
+# print(f"The circumference of the circle is {math.ceil(circum)}")
+
+# Area of a circle
+# radious = float(input("Enter the radius of the circle: "))
+# # area = math.pi * radious * radious
+# area = math.pi * pow(radious,2)
+# print(f"The circumference of the circle is {area} cm²")
+# print(f"The circumference of the circle is {round(area,3)}cm²")
+# print(f"The circumference of the circle is {math.floor(area)}cm²")
+# print(f"The circumference of the circle is {math.ceil(area)}cm²")
+
+# Calculate hypotenuse
+
+# perpendicular = int(input("Enter the perpendicular the triangle: "))
+# base = int(input("Enter the base of the triangle: "))
+# hypotenuse=math.sqrt(pow(perpendicular,2)+pow(base,2))
+# print(f"Hypotenuse = {round(hypotenuse,2)}")
+
+# Calculate Area of Rectangle
+length = float(input("Enter the length: "))
+breadth = float(input("Enter the breadth: "))
+area = length * breadth
+print(f"Area of your rectangle : {area}")
