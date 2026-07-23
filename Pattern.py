6205281c126f20aebceacd5055eee308cb159faa
Aman-Ali-03 <sayyedamanali1340@gmail.com>
2026-07-23 23:16:28 +0530

    Something Strange.

---
+++ Pattern.py
@@ -0,0 +1,13 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+     print(j,end="")
+
+    for k in range(i,5):
+      print(" ",end="")
+
+    for l in range(5,i,-1):
+        print(" ",end="")
+
+    for m in  range(1,i+1):
+        print(m,end="")
+    print()
